4ea5d8d81d5594b1c01e9bf2f77e5bb9f40d9375
kgw4it <30734393+kgw4it@users.noreply.github.com>
2018-09-28 18:19:07 +0200

    Added support for Alt+Shift+O
    Introduced a keyboard shortcut (Alt+Shift+O) that creates a secret link for the current selection or opens the secret creation page, if nothing is selected.

--- src/background_page/index.js
+++ src/background_page/index.js
@@ -6,28 +6,15 @@ import {
   Storage
 } from '../modules/storage'
 
-// Create OTS page context menu options
-function createContextItem() {
-  const contexts = ['page', 'link', 'selection']
 
-  browser.contextMenus.create({
-    id: '1',
-    title: 'Create secret link',
-    contexts: contexts
-  })
-
-  browser.contextMenus.onClicked.addListener(function(info, tab) {
-    var secret
-    if (info.selectionText) {
-      secret = info.selectionText
-    } else if (info.linkUrl) {
-      secret = info.linkUrl
+function processSecret(secret) {
+  var myStorage = Storage()
+  myStorage.init().then(function() {
+    if (!secret.length) {
+      browser.tabs.create({
+        url: myStorage.getHost()
+      });
     } else {
-      secret = info.pageUrl
-    }
-
-    var myStorage = Storage()
-    myStorage.init().then(function() {
       const ots = new OneTimeSecretApi(
         myStorage.getUsername(),
         myStorage.getApiKey(), {
@@ -56,20 +43,60 @@ function createContextItem() {
         .catch(function(error) {
           alert('Unable to create secret link: ' + error.message)
         })
-    })
+    }
   })
 }
 
+// Create OTS page context menu options
+function createContextItem() {
+  const contexts = ['page', 'link', 'selection']
+
+  browser.contextMenus.create({
+    id: '1',
+    title: 'Create secret link',
+    contexts: contexts
+  })
+
+  browser.contextMenus.onClicked.addListener(function(info, tab) {
+    var secret
+    if (info.selectionText) {
+      secret = info.selectionText
+    } else if (info.linkUrl) {
+      secret = info.linkUrl
+    } else {
+      secret = info.pageUrl
+    }
+    processSecret(secret);
+  });
+
+}
 // add the context buttons
 createContextItem()
 
 // register a handler for the icon click
-
 browser.browserAction.onClicked.addListener(function(tab) {
   var myStorage = Storage()
   myStorage.init().then(function() {
-    chrome.tabs.create({
+    browser.tabs.create({
       url: myStorage.getOpenSettingsOnIconClick() ? "options.html" : myStorage.getHost()
     });
   });
+});
+
+// register a handler for our commands
+browser.commands.onCommand.addListener(function(command) {
+  switch (command) {
+    case 'create-secret-from-selection':
+      browser.tabs.executeScript({
+          code: "window.getSelection().toString();"
+        })
+        .then(function(selection) {
+          var secret = selection[0];
+          processSecret(secret);
+        })
+        .catch(function(err) {
+          console.log(err);
+        });
+      break;
+  }
 });
